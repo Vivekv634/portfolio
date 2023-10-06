c0eba9355c12469a4581401253203d14d6fc9783
Vivek Vaish <vaishvivek634@gmail.com>
2023-10-06 22:20:51 +0530

    removing the router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-      </Routes>
+      <Home />
     </BrowserRouter>
   );
 }
